9d3771fcf9482ee58f8fbcb6eb661b516987fc10
RogerAlvarezM <roger.alvarez@fundacion-jala.org>
2019-02-21 11:21:23 -0400

    Task5:corrections

--- vagrant_jenkins/install_jenkins.sh
+++ vagrant_jenkins/install_jenkins.sh
@@ -6,11 +6,12 @@ echo "~ Installing Docker-compose ~"
 sudo apt-get install docker-compose -y
 echo "~ Installing java 8 ~"
 #install java
-sudo apt update
+sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
+java -version
 echo "~ Installing jenkins ~"
 #install jenkins:
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list
-sudo apt update
-sudo apt install jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins=2.150.3 -y
