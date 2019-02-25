8c13d8f4ad3871a8ddddb258c9519f99be765e76
RogerAlvarezM <roger.alvarez@fundacion-jala.org>
2019-02-25 11:38:46 -0400

    Task-7:Solve request

--- vagrant_docker_compose/install.sh
+++ vagrant_docker_compose/install.sh
@@ -1,6 +1,2 @@
-#install jenkins version 2.150.3
-docker pull registry.jala.info/devops/jenkins/jenkins:2.150.3-alpine
-cd docker
-mkdir jenkins_home
-chmod 777 jenkins_home
-cd ..
+mkdir -p docker/jenkins_home
+chmod 777 docker/jenkins_home
